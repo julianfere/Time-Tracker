f50b6350c23aac2dffa7203cecf0cb36b6ab2cb2
Julian Feregotti <julian.feregotti@snappler.com>
2022-09-09 11:14:23 -0300

    a

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,7 @@
-import "./index.scss";
+import { useState } from "react";
 import { Card } from "../../components/card";
 import data from "../../data.json";
+import "./index.scss";
 
 type JsonData = {
   title: string;
@@ -20,20 +21,48 @@ type JsonData = {
   };
 };
 
-const time = data as JsonData[];
+enum Time {
+  Daily = "daily",
+  Weekly = "weekly",
+  Monthly = "monthly",
+}
 
 const MainPage = () => {
-  const cards = time.map((item, index) => (
+  const [time, setTime] = useState<Time>(Time.Daily);
+
+  const cards = (data as JsonData[]).map((item, index) => (
     <Card
       key={index}
       title={item.title}
-      bodyContent={`${item.timeframes.daily.current}hrs`}
-      footerContent={`${item.timeframes.daily.previous}hrs`}
+      bodyContent={`${item.timeframes[time].current}hrs`}
+      footerContent={`${item.timeframes[time].previous}hrs`}
       type={item.title.toLowerCase()}
     />
   ));
 
-  return <div className="main">{cards}</div>;
+  const timeButtons = Object.values(Time).map((item, index) => (
+    <button
+      key={index}
+      className={time === item ? "btn active" : ""}
+      onClick={() => setTime(item)}
+    >
+      {item}
+    </button>
+  ));
+
+  return (
+    <main className="main-page">
+      <section className="main-grid">
+        <div className="header-container">
+          <section className="main-header">
+            <h1>Time Tracking</h1>
+          </section>
+          <section className="time-buttons">{timeButtons}</section>
+        </div>
+        {cards}
+      </section>
+    </main>
+  );
 };
 
 export default MainPage;
